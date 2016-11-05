b1730803631a6f54d5bd3a0bc60958767218c085
leonardosimoura <leonardosimoura@gmail.com>
2016-11-05 12:39:25 -0200

    commit

--- AppTemplate.UI.MVC/Startup.cs
+++ AppTemplate.UI.MVC/Startup.cs
@@ -19,7 +19,7 @@ namespace AppTemplate.UI.MVC
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = builder.Build(); 
         }
 
         public IConfigurationRoot Configuration { get; }
